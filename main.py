778fdbfed53434b5a8dfc875f5e37059fc678988
BrenoCRSilva <brenocrs@gmail.com>
2024-06-18 20:03:11 -0300

    Some changes

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pandas as pd
 
 def main():
-    
     df = pd.read_csv("data.csv")
-    print(df.to_string())
+    df["data"]= pd.to_datetime(df["data"], infer_datetime_format=True)
+    filtered_df = df.loc[(df['data'].dt.month == 6) & (df['data'].dt.year == 2016)]
+    print(filtered_df)
 
 
-def get_data_by_date():
-    pass
+
+def get_user_input():
+    try:
+        year = int(input("Enter year: "))
+        month = int(input("Enter month: "))
+        return year, month
+    except:
+        print("Invalid input. Please enter a valid year and month.")
+
 
 main()
