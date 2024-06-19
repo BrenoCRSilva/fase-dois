e0e2b8388c60cbd6de740a9ed97df3c2d45ab2ca
BrenoCRSilva <brenocrs@gmail.com>
2024-06-19 02:08:25 -0300

    Allowed input to be a range of dates

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ def main():
     year = get_year_input(first_year, last_year)
     df = read_csv(data_path)
     get_datetime(df, column, date_format)
-    filtered_df = filter_data(df, column, year, month)
+    filtered_df = filter_data(df, column, year[0], year[1], month[0], month[1])
     print(filtered_df)
-    
-
 def read_csv(file):
     return pd.read_csv(file)
 
@@ -24,9 +22,10 @@ def get_datetime(df, column, date_format):
     
 def get_month_input():
     try:
-        month = int(input("Insira o mes: "))
-        if month >= 1 and month <= 12:
-            return month
+        f_m = int(input("Insira o mes de inicio: "))
+        l_m = int(input("Insira o mes de termino: "))
+        if f_m >= 1 and f_m <= 12 and l_m >= 1 and l_m <= 12:
+            return [f_m, l_m]
         else:
             print("Invalid input. Please enter a valid month.")
             return get_month_input()
@@ -35,17 +34,25 @@ def get_month_input():
 
 def get_year_input(first_year, last_year):
     try:
-        year = int(input("Insira o ano: "))
-        if year >= first_year and year <= last_year:
-            return year
+        f_y = int(input("Insira o ano de inicio: "))
+        l_y = int(input("Insira o ano de termino: "))
+        if f_y >= first_year and f_y <= last_year and l_y >= first_year and l_y <= last_year:
+            return [f_y, l_y]
         else:
             print("Invalid input. Please enter a valid year.")
             return get_year_input(first_year, last_year)
     except:
         print("Invalid input. Please enter a valid year.")
 
-def filter_data(df, column, y, m):
-    filtered_df = df.loc[(df[column].dt.month >= m) & (df[column].dt.year >= y)] #add first and last conditions to loc
-    return filtered_df
+def filter_data(df, column, first_year, last_year, m, l):
+    if m < 10 and l < 9:
+        df = df[(df[column] >= f'{first_year}-0{m}-01') & (df[column] < f'{last_year}-0{l+1}-01')]
+    elif m < 10 and l >= 9:
+        df = df[(df[column] >= f'{first_year}-0{m}-01') & (df[column] < f'{last_year}-{l+1}-01')]
+    elif m >= 10 and l < 9:
+        df = df[(df[column] >= f'{first_year}-{m}-01') & (df[column] < f'{last_year}-0{l+1}-01')]
+    else:
+        df = df[(df[column] >= f'{first_year}-{m}-01') & (df[column] < f'{last_year}-{l+1}-01')]
+    return df
     
 main()
