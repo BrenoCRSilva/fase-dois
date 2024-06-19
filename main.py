d1073a306e7755e5394169023135eb607e15cf2a
BrenoCRSilva <brenocrs@gmail.com>
2024-06-19 17:06:16 -0300

    Changed variables names for better clarity

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ import pandas as pd
 
 def main():
     data_path = "data.csv"
-    column = "data"
+    date_column = "data"
     date_format = "%d/%m/%Y"
     first_year = 1961
     last_year = 2016
     
     print("---- Bem vindo ----\n\n\n")
     df = read_csv(data_path)
-    month = get_month_input()
-    year = get_year_input(first_year, last_year)
+    month_input = get_month_input()
+    year_input = get_year_input(first_year, last_year)
     print("Deseja ver: 1) todos os dados, 2) apenas os de precipitação, 3) apenas os de temperatura, ou 4) apenas os de umidade e vento?\n")
-    mode = get_mode_input()
-    get_datetime(df, column, date_format)
-    df_dates = filter_by_date(df, column, year[0], year[1], month[0], month[1])
-    filtered_df = filter_by_mode(df_dates,column, mode)
+    mode_input = get_mode_input()
+    get_datetime(df, date_column, date_format)
+    df_dates = filter_by_date(df, date_column, year_input[0], year_input[1], month_input[0], month_input[1])
+    filtered_df = filter_by_mode(df_dates, date_column, mode_input)
     print(f"\n{get_max_value(df, first_year, last_year)}")
     print(f"\n\n\n{filtered_df.to_string(index=False)}")
+    
 def read_csv(file):
     return pd.read_csv(file)
 
@@ -27,10 +28,10 @@ def get_datetime(df, column, date_format):
     
 def get_month_input():
     try:
-        f_m = int(input("Insira o mes de inicio: "))
-        l_m = int(input("Insira o mes de termino: "))
-        if f_m >= 1 and f_m <= 12 and l_m >= 1 and l_m <= 12:
-            return [f_m, l_m]
+        first_input = int(input("Insira o mes de inicio: "))
+        last_input = int(input("Insira o mes de termino: "))
+        if first_input >= 1 and first_input <= 12 and last_input >= 1 and last_input <= 12:
+            return [first_input, last_input]
         else:
             print("Invalid input. Please enter a valid month.")
             return get_month_input()
@@ -49,40 +50,40 @@ def get_mode_input():
         return get_mode_input()
 def get_year_input(first_year, last_year):
     try:
-        f_y = int(input("Insira o ano de inicio: "))
-        l_y = int(input("Insira o ano de termino: "))
-        if f_y >= first_year and f_y <= last_year and l_y >= first_year and l_y <= last_year:
-            return [f_y, l_y]
+        first_input = int(input("Insira o ano de inicio: "))
+        last_input = int(input("Insira o ano de termino: "))
+        if first_input >= first_year and first_input <= last_year and last_input >= first_year and last_input <= last_year:
+            return [first_input, last_input]
         else:
             print("Invalid input. Please enter a valid year.")
             return get_year_input(first_year, last_year)
     except:
         print("Invalid input. Please enter a valid year.")
 
-def filter_by_date(df, column, first_year, last_year, m, l):
-    if l == 12 and m >=10:
-        df = df[(df[column] >= f'{first_year}-{m}-01') & (df[column] < f'{last_year + 1}-01-01')]
-    elif l == 12 and m < 10:
-        df = df[(df[column] >= f'{first_year}-0{m}-01') & (df[column] < f'{last_year + 1}-01-01')]
-    elif m < 10 and l < 9:
-        df = df[(df[column] >= f'{first_year}-0{m}-01') & (df[column] < f'{last_year}-0{l+1}-01')]
-    elif m < 10 and l >= 9:
-        df = df[(df[column] >= f'{first_year}-0{m}-01') & (df[column] < f'{last_year}-{l+1}-01')]
-    elif m >= 10 and l < 9:
-        df = df[(df[column] >= f'{first_year}-{m}-01') & (df[column] < f'{last_year}-0{l+1}-01')]
+def filter_by_date(df, date_column, first_year, last_year, first_month, last_month):
+    if last_month == 12 and first_month >=10:
+        df = df[(df[date_column] >= f'{first_year}-{first_month}-01') & (df[date_column] < f'{last_year + 1}-01-01')]
+    elif last_month == 12 and first_month < 10:
+        df = df[(df[date_column] >= f'{first_year}-0{first_month}-01') & (df[date_column] < f'{last_year + 1}-01-01')]
+    elif first_month < 10 and last_month < 9:
+        df = df[(df[date_column] >= f'{first_year}-0{first_month}-01') & (df[date_column] < f'{last_year}-0{last_month + 1}-01')]
+    elif first_month < 10 and last_month >= 9:
+        df = df[(df[date_column] >= f'{first_year}-0{first_month}-01') & (df[date_column] < f'{last_year}-{last_month + 1}-01')]
+    elif first_month >= 10 and last_month < 9:
+        df = df[(df[date_column] >= f'{first_year}-{first_month}-01') & (df[date_column] < f'{last_year}-0{last_month + 1}-01')]
     else:
-        df = df[(df[column] >= f'{first_year}-{m}-01') & (df[column] < f'{last_year}-{l+1}-01')]
+        df = df[(df[date_column] >= f'{first_year}-{first_month}-01') & (df[date_column] < f'{last_year}-{last_month + 1}-01')]
     return df
     
-def filter_by_mode(df, column, mode):
+def filter_by_mode(df, date_column, mode):
     if mode == 1:
         return df
     elif mode == 2:
-        return df[[column, "precip"]]
+        return df[[date_column, "precip"]]
     elif mode == 3:
-        return df[[column, "temp_media"]]
+        return df[[date_column, "temp_media"]]
     else:
-        return df[[column,"um_relativa", "vel_vento"]]
+        return df[[date_column,"um_relativa", "vel_vento"]]
     
 def get_max_value(df, first_year, last_year):
     precip_dict = {}
