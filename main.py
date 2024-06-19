1ce17cdc540e2cff9e6e31c548983d68ba618c8d
BrenoCRSilva <brenocrs@gmail.com>
2024-06-19 00:32:43 -0300

    Implemented input validation and date filtering function

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 import pandas as pd
 
 def main():
-    df = pd.read_csv("data.csv")
-    df["data"]= pd.to_datetime(df["data"], infer_datetime_format=True)
-    filtered_df = df.loc[(df['data'].dt.month == 6) & (df['data'].dt.year == 2016)]
+    print("---- Bem vindo ----\n\n\n")
+    data_path = "data.csv"
+    column = "data"
+    date_format = "%d/%m/%Y"
+    first_year = 1961
+    last_year = 2016
+    month = get_month_input()
+    year = get_year_input(first_year, last_year)
+    df = read_csv(data_path)
+    get_datetime(df, column, date_format)
+    filtered_df = filter_data(df, column, year, month)
     print(filtered_df)
+    
 
+def read_csv(file):
+    return pd.read_csv(file)
 
-
-def get_user_input():
+def get_datetime(df, column, date_format):
+    df[column]= pd.to_datetime(df[column], format=date_format)
+    return df[column]
+    
+def get_month_input():
     try:
-        year = int(input("Enter year: "))
-        month = int(input("Enter month: "))
-        return year, month
+        month = int(input("Insira o mes: "))
+        if month >= 1 and month <= 12:
+            return month
+        else:
+            print("Invalid input. Please enter a valid month.")
+            return get_month_input()
     except:
-        print("Invalid input. Please enter a valid year and month.")
+        print("Invalid input. Please enter a valid month.")
 
+def get_year_input(first_year, last_year):
+    try:
+        year = int(input("Insira o ano: "))
+        if year >= first_year and year <= last_year:
+            return year
+        else:
+            print("Invalid input. Please enter a valid year.")
+            return get_year_input(first_year, last_year)
+    except:
+        print("Invalid input. Please enter a valid year.")
 
+def filter_data(df, column, y, m):
+    filtered_df = df.loc[(df[column].dt.month >= m) & (df[column].dt.year >= y)] #add first and last conditions to loc
+    return filtered_df
+    
 main()
